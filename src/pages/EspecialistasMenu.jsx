402bcd7d6844e4cf4a400ed6fafa88a9f4261e3f
Janderson <jandersonrodrigues216@gmail.com>
2025-01-20 13:11:00 -0300

    novo commit alteração do readme

--- src/pages/EspecialistasMenu.jsx
+++ src/pages/EspecialistasMenu.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Especialidade1 from "../images/especialidade1.png";
-import Especialidade2 from "../images/especialidade2.png";
-import Especialidade3 from "../images/especialidade3.png";
+import Especialidade2 from "../images/especialidade2.png"
+import Especialidade3 from "../images/especialidade3.png"
 import Especialidade4 from "../images/especialidade4.png"
+import Especialidade5 from "../images/saude-mental.gif"
+
 
 
 
@@ -10,7 +11,7 @@ const EspecialistasMenu = () => {
   return (
     <div className="flex justify-center gap-8 pt-5 w-full overflow-auto">
   <div className="flex flex-col items-center">
-    <img src={Especialidade1} alt="Psicologia Organizacional" className="w-24 h-24 object-cover" />
+    <img src={Especialidade5} alt="Psicologia Organizacional" className="w-24 h-24 object-cover" />
     <p className="text-center mt-2">Psicologia Organizacional</p>
   </div>
   <div className="flex flex-col items-center">
