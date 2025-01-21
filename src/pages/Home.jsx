6442c841766ad6a763c6abe4640fa18688eecf83
Janderson <jandersonrodrigues216@gmail.com>
2025-01-21 13:44:43 -0300

    alteração na navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../componets/Header'
 import EspecialistasMenu from './EspecialistasMenu'
 import Body from './Body'
+import Footer from '../componets/Footer'
 
 const Home = () => {
   return (
