ada605047ecf844a348fbc43ab9bc2fc34319198
Janderson <jandersonrodrigues216@gmail.com>
2025-01-22 18:22:47 -0300

    alteração no header

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import Psicologos from "../images/psicologo.png"
 
 const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap bg-primary rouded-lg px-6 md:px-10 lg:px-20'>
 
-      {/*-----Left Side----- */ }
+      
       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
         <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
         Plataforma de Consulta de psicologos <br /> Com profissionais de confiança.
@@ -19,9 +20,9 @@ const Header = () => {
             Marcar consulta <img className='w-3' src={assets.arrow_icon} alt="" />
         </a>
       </div>
-      {/*-----Right Side----- */}
+      
       <div className='md:w-1/2 relative '>
-        <img className='w-full md:absolute bottom-0 h-auto roudend-lg' src={assets.header_img} alt="" />
+        <img className='w-full md:absolute bottom-0 h-auto roudend-lg' src={Psicologos} alt="" />
       </div>
     </div>
   )
