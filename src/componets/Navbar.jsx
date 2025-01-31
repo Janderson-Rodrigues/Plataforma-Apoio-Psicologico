94452af47cac36da0239c85b739bc2f5295e5d96
Janderson <jandersonrodrigues216@gmail.com>
2025-01-31 17:41:05 -0300

    alteração na navbar

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -31,8 +31,8 @@ const Navbar = () => {
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
 
-        <NavLink to="/contact">
-          <li className="py-1">CADASTRO</li>
+        <NavLink to="/cadastro">
+          <li className="py-1">SOU PROFISSIONAL</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
