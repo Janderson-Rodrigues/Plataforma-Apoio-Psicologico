04e4a028776e9c6387a9630d6c5a568ecdbb0281
Anildo Gomes <anildo@outlook.com.br>
2025-01-31 09:01:19 -0300

    atualização pagina psicologos

--- src/pages/Psicologos.jsx
+++ src/pages/Psicologos.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
+import ProfissionaisData from "../data/profissionais.json";
+import "./styles/psicologos.css";
 
-const Psicologos = () => {
+function Psicologos() {
   return (
-    <div>
-      <p>Pagina psicologos</p>
-
-    </div>
-  )
+    <>
+      <div>
+        <ul>
+          {ProfissionaisData.map((profissional) => (
+            <li key={profissional.id}>
+              <h2>{profissional.nome}</h2>
+              <p>Especialidades:{profissional.especialidade}</p>
+              <p>Preço: R${profissional.preco}</p>
+              <img
+                src={profissional.imagem}
+                alt={profissional.nome}
+                className="psicologo-image"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default Psicologos
+export default Psicologos;
