01b5c21856812fead7c360f76f7efec0c09ccb0d
LFelipedev <luiz.fma.up@gmail.com>
2025-03-25 14:21:32 -0300

    :construction:

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -4,22 +4,22 @@ import Psicologos from "../images/psicologo.png"
 
 const Header = () => {
   return (
-    <div className='flex bg-primary '>
+    <div className='flex flex-col md:flex-row flex-wrap bg-primary px-6 md:px-10 lg:px-20'>
       <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
         <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
         Plataforma de Consulta de psicologos <br /> Com profissionais de confiança.
         </p>
         <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
             <img className='w-28' src={assets.group_profiles} alt="" />
-            <p className='text-[18px]'>A Plataforma de Apoio Psicológico Comunitário é uma solução inovadora que conecta profissionais de psicologia a pacientes que buscam suporte emocional, oferecendo atendimentos gratuitos ou a preços acessíveis. Por meio de um ambiente digital amigável.</p>
+            <p>A Plataforma de Apoio Psicológico Comunitário é uma solução inovadora que conecta profissionais de psicologia a pacientes que buscam suporte emocional, oferecendo atendimentos gratuitos ou a preços acessíveis. Por meio de um ambiente digital amigável.</p>
         </div>
-        {/* Botao marcar consulta */}
-        <a href="#especialistas" className='flex items-center gap-2 bg-white px-8  py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-125 transition-all duration-200'>
+         {/* Botao marcar consulta */}
+        <a href="#especialistas" className='flex items-center gap-2 bg-white px-8  py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-125 transition-all duration-300'>
             Marcar consulta <img className='w-3' src={assets.arrow_icon} alt="" />
         </a>
       </div>
     </div>
   )
-}
+};
 
 export default Header;
