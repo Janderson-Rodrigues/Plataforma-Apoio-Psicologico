af40b6e769eba9671960c1fcf66c1f2984f57c38
Janderson <jandersonrodrigues216@gmail.com>
2025-03-27 13:03:04 -0300

    alteração no texto

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,27 +33,14 @@ const LoginPage = () => {
       <div className="w-full md:w-1/2 flex flex-col justify-center py-12 px-6 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Se junte a nós!
+            Login
           </h2>
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-6 sm:rounded-lg sm:px-10">
             <form className="space-y-6">
-              <div>
-                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-                  Nome:
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
+             
 
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -90,7 +77,7 @@ const LoginPage = () => {
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Vamos lá!
+                  Fazer Login
                 </button>
               </div>
             </form>
