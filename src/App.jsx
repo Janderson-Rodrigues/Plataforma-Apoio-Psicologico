c73b707fd21e36a993676867d105c33a45cd9dae
LFelipedev <luiz.fma.up@gmail.com>
2025-03-27 14:29:13 -0300

    :construction: revertendo alteração que dá erro, caminho da página Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-//import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Myprofile from "./pages/Myprofile";
 import MyAppointment from "./pages/MyAppointment";
 import Appointment from "./pages/Appointment";
